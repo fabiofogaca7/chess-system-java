513598acf5b3bbc3d5e091f005f6197041944485
Fabio Fogaça <fabio.fogaca10@gmail.com>
2020-06-23 19:56:57 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Position pos = new Position(5, 3);
+		System.out.println(pos);
+		
 	}
 
 }
